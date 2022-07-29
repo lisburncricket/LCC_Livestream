ec22c037d633956ba1c782364281047687812a05
lisburncricket <110184312+lisburncricket@users.noreply.github.com>
2022-07-29 17:02:06 +0100

    Fixing title

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Lisburn CC Livestream</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Lisburn CC Livestreams" />
+        <Header title="Lisburn CC Livestream" />
         <p className="description">
           <iframe src="https://player.twitch.tv/?channel=lisburncricket&parent=www.lisburncricket.club&parent=lisburncricket.club" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
         </p>
