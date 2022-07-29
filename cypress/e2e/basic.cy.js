0636a3c4cc858e72e92c1995760f3058ba0035a2
lisburncricket <110184312+lisburncricket@users.noreply.github.com>
2022-07-29 15:42:58 +0100

    Removing footer test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -2,12 +2,4 @@ describe('sample test', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
-  it('renders the Netlify logo image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
 })
